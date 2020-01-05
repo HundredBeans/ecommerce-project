a5777123caeda5a369a1363871d713bd6c923d82
Mohammad Daffa <daffa@alterra.id>
2020-01-05 18:27:19 +0700

    add mock-up database and edit folder's structure

--- blueprints/keranjang/models.py
+++
@@ -1,4 +0,0 @@
-from blueprints import db
-from flask_restful import fields
-
-# Database Checkout menggunakan checkout status pada database keranjang
--- test.sh
+++
@@ -1,4 +0,0 @@
-clear && clear
-export FLASK_ENV=testing
-pytest --cov-fail-under=80 --cov=blueprints --cov-report html -s
-export FLASK_ENV=development
